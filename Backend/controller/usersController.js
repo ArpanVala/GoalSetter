4fb5c51f1fc43c08d630419d379a2281ec41c3dc
ArpanVala <valaarpan05@gmail.com>
2025-03-04 18:53:30 +0530

    login and registrartion endpoints implemented

--- Backend/controller/usersController.js
+++ Backend/controller/usersController.js
@@ -8,14 +8,68 @@ const bcrypt = require('bcryptjs');
 //@route GET /api/user/
 //@access Public
 const registerUser = asyncHandler(async(req,res)=>{
-    res.status(200).json({message:'user registered!'})
+    const {name, email, password} = req.body;
+
+    if(!name || !email || !password){
+        res.status(400);
+        throw new Error('Please fill all the fields')
+    }
+
+    //checj if user already exist
+    const userExists = await User.findOne({email})
+
+    if(userExists)
+    {
+        res.status(400);
+        throw new Error('User already exists')
+    }
+    //hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    //create new user
+    const user = await User.create({
+        name,
+        email,
+        password:hashedPassword,
+    })
+
+    if(user)
+    {
+        res.status(201).json({
+            _id:user.id,
+            name:user.name,
+            email:user.email,
+        })
+    }
+    else
+    {
+        res.status(400)
+        throw new Error('Invalid user data');
+    }
 })
 
 //@desc login user
 //@route GET /api/user/login
 //@access Public
 const loginUser = asyncHandler(async(req,res)=>{
-    res.status(200).json({message:'user login!'})
+    const {email,password} = req.body;
+
+    const user = await User.findOne({email})
+
+    if(user && (await bcrypt.compare(password, user.password)))
+    {
+        res.status(200).json({
+            _id:user.id,
+            name:user.name,
+            email:user.email
+        })
+    }
+    else
+    {
+        res.status(400)
+        throw new Error('Invalid credentials !');
+    }
 })
 
 //@desc user data
