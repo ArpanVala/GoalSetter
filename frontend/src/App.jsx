7a884f85fe2daaec2e826ef85031d41cecf0d078
ArpanVala <valaarpan05@gmail.com>
2025-03-05 21:12:57 +0530

    routes implemented

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import DashboardPage from './pages/DashboardPage'
+import LoginPage from './pages/LoginPage'
+import RegisterPage from './pages/RegisterPage'
+import Header from './components/Header'
 
 const App = () => {
   return (
-    <div className='container'>
-      <h1>Hi There !</h1>
+    <>
+    <BrowserRouter>
+    <div className="container">
+    <Header/>
+    <Routes>
+      <Route path='/' element={<DashboardPage/>}/>
+      <Route path='/login' element={<LoginPage/>}/>
+      <Route path='/register' element={<RegisterPage/>}/>
+    </Routes>
     </div>
+    </BrowserRouter>
+
+    </>
+
   )
 }
 
