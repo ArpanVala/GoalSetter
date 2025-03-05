8b4503c04bbe19d828cd582d8a1d957ead92a361
ArpanVala <valaarpan05@gmail.com>
2025-03-05 22:58:44 +0530

    login & register frontend added

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,8 +1,53 @@
+import  {useState, useEffect} from 'react'
+import {FaSignInAlt} from 'react-icons/fa'
+import {Link} from 'react-router-dom'
+
 const LoginPage = () => {
+    const [formData, setFormData] = useState({
+        email:'',
+        password:'',
+    })
+
+    const {email, password} = formData
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }))
+  }
+
+    const onSubmit = (e) => {
+        e.preventDefault()
+    }
   return (
-    <div>
-      Login Page
-    </div>
+    <>
+     <section className="heading">
+        <h4><FaSignInAlt/>Login</h4>
+        <p>Login and make goals !</p>
+     </section>
+
+     <section className="form">
+        <form onSubmit={onSubmit}>
+    
+            <div className="form-group">
+            <input type='text' className='form-control' id='email' name='email' value={email} placeholder='Enter email' 
+            onChange={onChange}/>
+            </div>
+
+            <div className="form-group">
+            <input type='password' className='form-control' id='password' name='password' value={password} placeholder='Enter Password' 
+            onChange={onChange}/>
+            </div>
+
+            <div className='form-group'>
+            <button type='submit' className='btn btn-block'>
+              Login
+            </button>
+          </div>
+        </form>
+     </section>
+    </>
   )
 }
 
