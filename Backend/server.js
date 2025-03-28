751031bb81ab606a9eef03c721f4a566785eb895
ArpanVala <valaarpan05@gmail.com>
2025-03-28 20:19:26 +0530

    redux added for register

--- Backend/server.js
+++ Backend/server.js
@@ -6,8 +6,9 @@ const {connectDB} = require('./config/db');
 
 connectDB();
 
-const port = process.env.PORT || 8001;
+const port = process.env.PORT || 5000;
 const app=express(); 
+
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
