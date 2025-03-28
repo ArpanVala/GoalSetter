c303297fde3a92ffc46d089310bba2843a670e56
ArpanVala <valaarpan05@gmail.com>
2025-03-28 22:07:03 +0530

    spinner component added

---
+++ frontend/src/components/Spinner.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Spinner = () => {
+  return (
+    <div className='loadingSpinnerContainer'>
+      <div className="loadingSpinne">
+
+      </div>
+    </div>
+  )
+}
+
+export default Spinner
