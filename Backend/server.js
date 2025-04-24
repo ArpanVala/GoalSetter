21c6e9b5196f8ed6c491211e9224c4ed5b5d292f
ArpanVala <valaarpan05@gmail.com>
2025-04-24 20:53:06 +0530

    all set to deploy

--- Backend/server.js
+++ Backend/server.js
@@ -1,3 +1,4 @@
+const  path = require('path');
 const express = require('express');
 const dotenv =  require('dotenv').config();
 const {errorHandler} = require('./middleware/errorMiddleware');
@@ -16,7 +17,17 @@ app.use(express.urlencoded({extended:true}));
 app.use('/api/goals',require('./router/goalsRouter'));
 app.use('/api/users',require('./router/usersRouter'));
 
+//serve frontend
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname,'../frontend/dist')));
+    app.get('*',(req,res)=>res.sendFile(path.resolve(__dirname,'../','frontend','dist','index.html')))
+}
+else{
+    app.get('/',(req,res)=>res.send("please go to production"))
+}
+
 app.use(errorHandler);
 app.listen(port,()=>{console.log(`Server is running on port ${port}...`);
 })
+
 
